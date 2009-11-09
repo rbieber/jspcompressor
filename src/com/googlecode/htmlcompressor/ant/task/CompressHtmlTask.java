e45bed4306464ac6974ab8bb65a322d76f70491b
Ron Bieber <ron@bieberlabs.com>
2009-11-09 06:37:58 -0600

    Add ant task to main JAR file

---
+++ src/com/googlecode/htmlcompressor/ant/task/CompressHtmlTask.java
@@ -0,0 +1,160 @@
+package com.googlecode.htmlcompressor.ant.task;
+
+import org.apache.tools.ant.Task;
+import com.googlecode.htmlcompressor.compressor.*;
+import java.io.*;
+import java.util.Vector;
+import java.util.Enumeration;
+import org.apache.tools.ant.types.FileSet;
+import org.apache.tools.ant.DirectoryScanner;
+
+public class CompressHtmlTask extends Task {
+    private Vector filesets = new Vector();
+    private String destdir = null;
+    
+    public void setDestDir(String s) {
+        destdir = s;
+    }
+
+    public void execute() {
+       
+        if (filesets.size() == 0) {
+            return;
+        }
+        
+        Enumeration e = filesets.elements();
+        
+        while (e.hasMoreElements()) {
+            FileSet fs = (FileSet) e.nextElement();
+            DirectoryScanner ds = fs.getDirectoryScanner(getProject());
+            
+            String[] includedFiles = ds.getIncludedFiles();
+
+            log("Compressing " + includedFiles.length + " files to " + this.destdir + " ...");
+                
+            
+            for (int i = 0; i < includedFiles.length; i++) {
+                File f = new File(ds.getBasedir(), includedFiles[i]);
+                File d = new File(destdir, includedFiles[i]);
+                
+                File p = new File(d.getParent());
+                
+                if (!p.exists()) {
+                    p.mkdirs();
+                }
+                
+                writeFile(d.toString(), compressHTML(readFile(f.toString(), null)), null);
+            }
+        
+        }
+    }
+       
+    private String compressHTML(String buffer) {
+        HtmlCompressor compressor = new HtmlCompressor();
+        String newHTML = null;
+    
+        compressor.setEnabled(true); //if false all compression is off (default is true)
+        compressor.setRemoveComments(true); //if false keeps HTML comments (default is true)
+        compressor.setRemoveMultiSpaces(true); //if false keeps multiple whitespace characters (default is true)
+        compressor.setRemoveIntertagSpaces(true);//removes iter-tag whitespace characters
+        compressor.setRemoveQuotes(false); //removes unnecessary tag attribute quotes
+        compressor.setCompressCss(true); //compress css using Yahoo YUI Compressor
+        compressor.setCompressJavaScript(false); //compress js using Yahoo YUI Compressor
+        compressor.setYuiCssLineBreak(80); //--line-break param for Yahoo YUI Compressor
+        compressor.setYuiJsDisableOptimizations(true); //--disable-optimizations param for Yahoo YUI Compressor
+        compressor.setYuiJsLineBreak(-1); //--line-break param for Yahoo YUI Compressor
+        compressor.setYuiJsNoMunge(false); //--nomunge param for Yahoo YUI Compressor
+        compressor.setYuiJsPreserveAllSemiColons(true);//--preserve-semi param for Yahoo YUI Compressor
+        
+        try {
+            newHTML = compressor.compress(buffer);
+        } catch (Exception q) {
+            q.printStackTrace();
+        }
+    
+        return(newHTML);
+    }
+     
+    private String readFile(String path, String encoding) {
+        String finalString = null;
+        try {
+            // summary: reads a file and returns a string
+            if (encoding == null) {
+                encoding = "utf-8";
+            }
+            
+            File file = new File(path);
+            
+            BufferedReader input = new java.io.BufferedReader(new InputStreamReader(new FileInputStream(file), encoding));
+            
+            try {
+                StringBuffer stringBuffer = new StringBuffer();
+                String line = input.readLine();
+                String lineSeparator = System.getProperty("line.separator");
+                
+                // Byte Order Mark (BOM) - The Unicode Standard, version 3.0, page
+                // 324
+                // http://www.unicode.org/faq/utf_bom.html
+                
+                // Note that when we use utf-8, the BOM should appear as "EF BB BF",
+                // but it doesn't due to this bug in the JDK:
+                // http://bugs.sun.com/bugdatabase/view_bug.do?bug_id=4508058
+                if (line != null && line.length() > 0 && line.charAt(0) == 0xfeff) {
+                    // Eat the BOM, since we've already found the encoding on this
+                    // file,
+                    // and we plan to concatenating this buffer with others; the BOM
+                    // should
+                    // only appear at the top of a file.
+                    line = line.substring(1);
+                }
+                
+                while (line != null) {
+                    stringBuffer.append(line);
+                    stringBuffer.append(lineSeparator);
+                    line = input.readLine();
+                }
+                
+                // Make sure we return a JavaScript string and not a Java string.
+                return stringBuffer.toString(); // String
+            } finally {
+                input.close();
+            }
+        } catch (Exception e) {
+        }
+        
+        return(null);
+    }
+    
+    
+    private String writeFile(String path, String contents, String encoding) {
+        String finalString = null;
+        try {
+            // summary: reads a file and returns a string
+            if (encoding == null) {
+                encoding = "utf-8";
+            }
+            
+            File file = new File(path);
+            String lineSeparator = System.getProperty("line.separator");
+            BufferedWriter output = new java.io.BufferedWriter(new OutputStreamWriter(new FileOutputStream(file), encoding));
+            try {
+                output.write(contents, 0, contents.length());
+            } finally {
+                output.flush();
+                output.close();
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        
+        return(null);
+    }
+    
+    /**
+     *  A set of files to upload or download
+     */
+    public void addFileset(FileSet set) {
+        filesets.addElement(set);
+    }
+
+}
